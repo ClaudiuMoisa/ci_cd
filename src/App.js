a4a94643432141be424836f84cb7a9032469d7a4
MoisaSyntax <claudiu.moisa@syntax.com>
2022-09-22 14:58:49 +0300

    Remove logo import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>CI/CD with Github actions</h2>
+      <h2>CI/CD with Github actions updated</h2>
     </div>
   );
 }

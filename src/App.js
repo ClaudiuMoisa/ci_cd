e1ed8252daac1d525b34007c18946ad373d5a2f5
MoisaSyntax <claudiu.moisa@syntax.com>
2022-09-22 15:16:22 +0300

    Update text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Github actions updated</h2>
+      <h2>Github actions</h2>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Github actions updated text', () => {
+test('renders Github actions text', () => {
   render(<App />);
-  const textElement = screen.getByText(/Github actions updated/i);
+  const textElement = screen.getByText(/Github actions/i);
   expect(textElement).toBeInTheDocument();
 });
